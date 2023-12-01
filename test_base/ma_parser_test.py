a357aaf10849bc83818b082e75d62ce03b34dd00
Val <valerii.filin@gmail.com>
2023-12-01 17:02:24 +0200

    TestMAParser: more informative logging

--- test_base/ma_parser_test.py
+++ test_base/ma_parser_test.py
@@ -1,4 +1,6 @@
 from test_base.any_parser_test import TestAnyParser
+from Magritte.descriptions.MAContainer_class import MAContainer
+from Magritte.visitors.MAJsonWriter_visitors import MAObjectJsonWriter, MAValueJsonWriter
 
 
 class TestMAParser(TestAnyParser):
@@ -61,22 +63,39 @@ class TestMAParser(TestAnyParser):
             },
         }
     '''
+    object_writer = MAObjectJsonWriter()
+    value_writer = MAValueJsonWriter()
     # In some cases it may be necessary to override _verify_parsed_items method
 
     def _verify_parsed_items(self) -> None:
         """Validates the parsed items against the expected items."""
         self.assertEqual(len(self.parsed_items), len(self.expected_data), "Number of parsed items doesn't match")
         item_num = 0
-        for item in self.parsed_items:
-            errs = self.expected_data[item_num][0].validate(item)
-            self.assertEqual(len(errs), 0, f"Item #{item_num} ({item!r}) validation failed: {errs}")
+        for parsed_item in self.parsed_items:
+            expected_desc = self.expected_data[item_num][0]
+            expected_item = self.expected_data[item_num][1]
+            errs = expected_desc.validate(parsed_item)
+            self.assertEqual(len(errs), 0, f"Item #{item_num} ({parsed_item!r}) validation failed: {errs}")
+            if isinstance(expected_desc, MAContainer):
+                try:
+                    parsed_str = self.object_writer.write_json(parsed_item, expected_desc)
+                except RecursionError:
+                    parsed_str = f"{parsed_item!r}(has cyclic references)"
+            else:
+                parsed_str = self.value_writer.write_json(parsed_item, expected_desc)
+            if isinstance(expected_desc, MAContainer):
+                try:
+                    expected_str = self.object_writer.write_json(expected_item, expected_desc)
+                except RecursionError:
+                    expected_str = f"{expected_item!r}(has cyclic references)"
+            else:
+                expected_str = self.value_writer.write_json(expected_item, expected_desc)
             self.assertTrue(
                 self.parser_meta["equality_tester"].equal(
-                    item,
-                    self.expected_data[item_num][1],
-                    self.expected_data[item_num][0],
+                    parsed_item,
+                    expected_item,
+                    expected_desc,
                     ),
-                f"Item #{item_num} ({item!r}) is not equal to the expected item "
-                f"({self.expected_data[item_num][1]!r})",
+                f"Parsed item #{item_num} ({parsed_str}) is not equal to the expected item ({expected_str})",
                 )
             item_num += 1
