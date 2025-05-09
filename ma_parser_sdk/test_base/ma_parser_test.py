6c7a35530a49cf60dbb9b4f118af734327b4ad3e
Val <valerii.filin@gmail.com>
2025-05-09 19:55:39 +0400

    Extended logging

--- ma_parser_sdk/test_base/ma_parser_test.py
+++ ma_parser_sdk/test_base/ma_parser_test.py
@@ -1,6 +1,10 @@
+import inspect
+import logging
 from ma_parser_sdk.test_base.any_parser_test import TestAnyParser
 from Magritte.descriptions.MAContainer_class import MAContainer
 from Magritte.visitors.MAJson_visitors import MAObjectJsonWriter, MAValueJsonWriter
+
+logger = logging.getLogger(__name__)
 
 
 class TestMAParser(TestAnyParser):
@@ -69,11 +73,16 @@ class TestMAParser(TestAnyParser):
 
     def _verify_parsed_items(self) -> None:
         """Validates the parsed items against the expected items."""
+        logger.info("Verifying parsed items...")
         self.assertEqual(len(self.parsed_items), len(self.expected_data), "Number of parsed items doesn't match")
         item_num = 0
         for parsed_item in self.parsed_items:
             expected_desc = self.expected_data[item_num][0]
             expected_item = self.expected_data[item_num][1]
+            logger.debug(
+                f"Validating parsed item #{item_num} ({list(filter(lambda x: not x[0].startswith('__'), inspect.getmembers(parsed_item)))}) "
+                f"against the expected item ({list(filter(lambda x: not x[0].startswith('__'), inspect.getmembers(expected_item)))})"
+                )
             errs = expected_desc.validate(parsed_item)
             self.assertEqual(len(errs), 0, f"Item #{item_num} ({parsed_item!r}) validation failed: {errs}")
             if isinstance(expected_desc, MAContainer):
@@ -90,6 +99,7 @@ class TestMAParser(TestAnyParser):
                     expected_str = f"{expected_item!r}(has cyclic references)"
             else:
                 expected_str = self.value_writer.write_json(expected_item, expected_desc)
+            logger.debug(f"Comparing parsed item #{item_num} ({parsed_str}) with the expected item ({expected_str})")
             self.assertTrue(
                 self.parser_meta["equality_tester"].equal(
                     parsed_item,
